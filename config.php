d91a37895602de3c13896ec400fdbe00eb455c65
CAndrei17 <36935789+CAndrei17@users.noreply.github.com>
2018-11-20 14:43:51 +0200

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/Proiect1/');
-    define('CART_COOKIE','ACjm1712DecZzZ');
-    define('CART_COOKIE_EXPIRE',time() + (86400 * 30));
-    define('TAXRATE',0.09);
