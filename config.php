d67d531fe734cc8bcf4c8b0818130467ef4cf386
CAndrei17 <36935789+CAndrei17@users.noreply.github.com>
2018-11-20 14:42:52 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+    define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/Proiect1/');
+    define('CART_COOKIE','ACjm1712DecZzZ');
+    define('CART_COOKIE_EXPIRE',time() + (86400 * 30));
+    define('TAXRATE',0.09);
